59529bf85a550348592594d12978c4a3e609b2e9
rlysamgold <abrite.od@gmail.com>
2015-10-31 23:38:16 +0100

    initial commit

---
+++ src/main/java/com/pugovkin/d/Main.java
@@ -0,0 +1,14 @@
+package com.pugovkin.d;
+
+/**
+ * Created by Abrite on 31.10.15.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world for GitHub");
+    }
+}
