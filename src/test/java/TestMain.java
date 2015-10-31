0b3d4afbb9c21ad360c36520d95e6c01df0b3cfd
rlysamgold <abrite.od@gmail.com>
2015-11-01 00:36:56 +0100

    working enviroment

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Abrite on 01.11.15.
+ */
+public class TestMain {
+
+
+    @Test
+    public void testMain() {
+
+        assertTrue(true);
+    }
+
+}
